3fa0d0241c18c169b34f7a531e36846bb8b42185
rizzkaaa <joremifasollasido@gmail.com>
2025-06-23 18:27:24 +0700

    ganti db

--- dashboard/dashboard-perusahaan/index.php
+++ dashboard/dashboard-perusahaan/index.php
@@ -102,7 +102,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         </div>
                     <?php else : ?>
                         <?php while ($l = mysqli_fetch_assoc($lowongans)) : ?>
-                            <?= cardLowongan($l) ?>
+                            <?= cardLowongan($l) ?> 
                         <?php endwhile; ?>
                     <?php endif; ?>
                 </div>
